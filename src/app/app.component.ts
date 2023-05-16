93bb089159733aa9565cdb566787e7ff0781be41
felipebuitrago <49107226+felipebuitrago@users.noreply.github.com>
2023-05-15 23:09:46 -0500

    added ejemplos y ui interactions con signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,16 @@
 import { Component, signal } from '@angular/core';
 
+interface MenuItem {
+  label?:string,
+  link?:string,
+}
+
+interface Usuario {
+  name:string,
+  uid:string,
+  role:string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,7 +19,20 @@ import { Component, signal } from '@angular/core';
 export class AppComponent {
   title = 'signals-angular';
 
-  notSignalArray = ["signal","array"];
-  signalArray = signal(["signal","array"]);
+  notSignalArray = ["signal","array"];//referencia/state forma normal
+
+  abrirMenu = signal(false);//señal de valor primitivo - boolean 
+  // simitar to react => const [abrirMenu,setAbrirMenu] = useState(false)
+  
+  signalArray = signal(["signal","array"]);//señal de arreglo de tipos primitivos
+
+  menuItems = signal<MenuItem[]>([//señal de arreglo de objetos de tipo MenuItem
+    {label:"Inicio",link:"/"},
+    {label:"Inventario",link:"/inventario"}
+  ])
+
+  usuario = signal<Usuario>(//señal de un objeto de tipo Usuario
+    {name:"felipe",role:"administrador",uid:"jh776dcb676ad"}
+  )
 
 }
